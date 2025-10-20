26fe12fd7b4fcb493eb0ebd301ff5e0595d8d11b
serkan <40214329+serayd61@users.noreply.github.com>
2025-10-20 16:41:40 +0200

    Refactor tailwind.config.js to TypeScript

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      backdropBlur: {
+        xs: '2px',
+      },
+    },
   },
   plugins: [],
 }
+export default config
